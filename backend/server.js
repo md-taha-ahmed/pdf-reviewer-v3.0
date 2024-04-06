a544ca24d00b517c3fb0a7a6a6a1bcef7cc97ecc
md-taha-ahmed <md.taha.ahmed.projects@gmail.com>
2024-04-06 04:03:36 +0400

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
-
+const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
 const articleRoutes = require('./routes/articles')
 
 // express app
 const app = express()
+app.use(cors())
 
 // middleware
 app.use(express.json())
